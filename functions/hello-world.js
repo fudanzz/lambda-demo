38e8279faba2a4326d17f24dc8caf511cf50f719
Zhao <zhongz@88e9fe7b82be.ant.amazon.com>
2019-09-14 21:30:50 +0800

    update hello-world

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -4,7 +4,7 @@ module.exports.handler = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!!!'
+      message: 'Go Serverless v1.0! Your function executed successfully!'
     }, null, 2),
   };
 
