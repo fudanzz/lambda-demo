99ea2b31fa1940a5d041fa2278315221a556b79f
Zhao <zhongz@88e9fe7b82be.ant.amazon.com>
2019-10-14 16:24:38 +0800

    add node image

--- build.sh
+++ build.sh
@@ -15,17 +15,17 @@ if [ $# -eq 0 ]; then
   instruction
   exit 1
 elif [ "$1" = "int-test" ] && [ $# -eq 1 ]; then
-  npm install
+  npm --registry https://registry.npm.taobao.org install
 
   npm run integration-test
 elif [ "$1" = "acceptance-test" ] && [ $# -eq 1 ]; then
-  npm install
+  npm --registry https://registry.npm.taobao.org install
 
   npm run acceptance-test
 elif [ "$1" = "deploy" ] && [ $# -eq 2 ]; then
   STAGE=$2
 
-  npm install
+  npm --registry https://registry.npm.taobao.org install
   'node_modules/.bin/sls' deploy -s $STAGE
 else
   instruction
